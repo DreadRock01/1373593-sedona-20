ca66a0541fed9ca0548a6fe24cb43d753b317f44
DreadRock01 <dreadrock01@users.noreply.github.com>
2020-09-04 11:18:39 +0500

    Скрипт мобильного меню с nojs

---
+++ source/js/script.js
@@ -0,0 +1,11 @@
+
+let nav = document.querySelector('.nav-list');
+let menuBtn = document.querySelector('.nav-list__toggle');
+
+nav.classList.add('nav-list--closed');
+menuBtn.classList.remove('nav-list__toggle--nojs');
+
+menuBtn.addEventListener('click', function() {
+  menuBtn.classList.toggle('nav-list__toggle--closed');
+  nav.classList.toggle('nav-list--closed');
+});
