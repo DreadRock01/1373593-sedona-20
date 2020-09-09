cffc631df7a404b9d865ad375fb956315ce0f7d8
DreadRock01 <dreadrock01@users.noreply.github.com>
2020-09-09 14:23:38 +0500

    Автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,36 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webP = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const del = require("del");
+
+// Сlean
+
+const clean = () => {
+  return del("build");
+};
+exports.clean = clean;
+
+// Copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/*.html"
+    ], {
+        base: "source"
+    })
+    .pipe(gulp.dest("build"));
+};
+
+exports.copy = copy;
 
 // Styles
 
@@ -16,8 +46,10 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
@@ -49,3 +81,51 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+// Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+    ]))
+};
+
+exports.images = images;
+
+// WebP images
+
+const webp = () => {
+  return gulp.src("source/img/**/*{jpg,png}")
+    .pipe(webP({quality: 90}))
+    .pipe(gulp.dest("source/img"))
+};
+
+exports.webp = webp;
+
+// SVG Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/**/icon-*.svg")
+  .pipe(imagemin([
+    imagemin.svgo({plugins: [
+      { removeAttrs: { attrs: 'fill' } }
+    ]})
+  ]))
+    .pipe(svgstore())
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"))
+};
+
+exports.sprite = sprite;
+
+const build = gulp.series(
+  clean,
+  copy,
+  styles,
+  sprite
+);
+
+exports.build = build;
